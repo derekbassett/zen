ff424b302ca574bc12bfea64a915f1829b6b3de2
Phil <zgrubby@gmail.com>
2017-03-01 14:42:07 +0800

    set parsed flag

--- context.go
+++ context.go
@@ -56,6 +56,7 @@ func (s *Server) putBackContext(c *Context) {
 
 // parseInput will parse request's form and
 func (c *Context) parseInput() error {
+	c.parsed = true
 	return c.Req.ParseForm()
 }
 
